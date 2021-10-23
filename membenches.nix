8318ffccfa739f5196966a8df84d0083614e3d6c
Michael Bishop <cleverca22@gmail.com>
2021-10-23 07:14:08 -0300

    post-process things on hydra

--- membenches.nix
+++ membenches.nix
@@ -5,7 +5,9 @@ let
   makeNLinks = k: v: lib.concatMapStringsSep "\n" (n: "ln -sv ${v.override { inherit n; }} $out/${k}-${toString n}") (lib.range 1 rerunCount);
   symlinks = builtins.attrValues (lib.mapAttrs makeNLinks variants);
 in
-runCommand "membenches" {} ''
+runCommand "membenches" {
+  preferLocalBuild = true;
+} ''
   mkdir -p $out/nix-support
   ${lib.concatStringsSep "\n" symlinks}
 
