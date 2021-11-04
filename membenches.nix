03faee81f011bfb680f2dda83747ad86415fd28f
Michael Bishop <cleverca22@gmail.com>
2021-11-04 03:04:01 -0300

    split processing up from merging

--- membenches.nix
+++ membenches.nix
@@ -8,30 +8,37 @@ let
       (currentIteration: "ln -sv ${v.override { inherit currentIteration; }} $out/${k}-${toString currentIteration}")
       (lib.range 1 nIterations);
   symlinks = builtins.attrValues (lib.mapAttrs makeNLinks variants);
-in
-runCommand "membenches" {
+  mergedResult = runCommand "membenches" {
+    preferLocalBuild = true;
+    nativeBuildInputs = [ jq ];
+  } ''
+    mkdir -p $out/nix-support
+    ${lib.concatStringsSep "\n" symlinks}
+
+    cd $out
+
+
+    cp ${./render_html.jq} render_html.jq
+    cat */refined.json | jq --slurp 'include "render_html"; . | render_html' --raw-output > $out/render.html
+
+    tar -cf alljson.tar */*.json */input/*.json */input/highwater */input/rts.dump */input/stderr
+    gzip -9v alljson.tar
+
+    cat > nix-support/hydra-build-products <<EOF
+    file binary-dist $out/alljson.tar.gz
+    report testlog $out render.html
+    EOF
+  '';
+in runCommand "post-processed" {
   preferLocalBuild = true;
-  nativeBuildInputs = [ jq ];
 } ''
   mkdir -p $out/nix-support
-  ${lib.concatStringsSep "\n" symlinks}
-
-  cd $out
+  cp ${mergedResult}/nix-support/* $out/nix-support
+  chmod -R +w $out
 
   cp -vr ${./bench} bench
   chmod -R +w bench
   patchShebangs bench
 
   #bench/process.sh
-
-  cp ${./render_html.jq} render_html.jq
-  cat */refined.json | jq --slurp 'include "render_html"; . | render_html' --raw-output > $out/render.html
-
-  tar -cf alljson.tar */*.json */input/*.json */input/highwater */input/rts.dump */input/stderr
-  gzip -9v alljson.tar
-
-  cat > nix-support/hydra-build-products <<EOF
-  file binary-dist $out/alljson.tar.gz
-  report testlog $out render.html
-  EOF
 ''
